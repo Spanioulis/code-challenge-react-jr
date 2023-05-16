9d75e21ddab9db94e683dfc31308fa4ef7e6f80b
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-16 10:11:52 +0200

    Inicio proyecto code-challenge

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+   return (
+      <div>
+         <h1>Code Challenge React Jr.</h1>
+         <p>Inicio prueba...</p>
+      </div>
+   );
+}
+
+export default App;
