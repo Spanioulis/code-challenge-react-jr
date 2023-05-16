3870656163f26dcf82e274d8d7ca9ad86df5a326
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-16 10:34:35 +0200

    feat: crea carpeta styles con colors & dimensions

---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+   primary: '#E51C20',
+   black: {
+      black1: '#222222',
+      black2: '#3A3A3A'
+   },
+   gray: {
+      gray1: '#333333',
+      gray2: '#636363',
+      gray3: '#7A7A7A',
+      gray4: '#E4E4E4'
+   },
+   white: '#FFFFFF'
+};
